0b3bc1c1e5f4228b1c37e52128714b7910858802
Gitit2meBaby <thisworldofdans@gmail.com>
2023-09-26 02:00:53 +0700

    Flip card not working, i have wasted way to much time on debugging this issue. maybe one day i will work it out, for now, this will have to do...

--- script.js
+++ script.js
@@ -1,43 +1,55 @@
-const days = document.querySelector('#days')
-const hours = document.querySelector('#hours')
-const minutes = document.querySelector('#minutes')
-const seconds = document.querySelector('#seconds')
+document.addEventListener('DOMContentLoaded', function () {
+    const days = document.querySelector('#days');
+    const hours = document.querySelector('#hours');
+    const minutes = document.querySelector('#minutes');
+    const seconds = document.querySelector('#seconds');
 
-let daysCount = 10
-let hoursCount = 12
-let minutesCount = 59
-let secondsCount = 59
+    const secondsContainer = document.querySelector('#secondsContainer');
+    const minutesContainer = document.querySelector('#minutesContainer');
+    const hoursContainer = document.querySelector('#hoursContainer');
 
-setInterval(countdown, 1000);
+    let daysCount = 10;
+    let hoursCount = 12;
+    let minutesCount = 59;
+    let secondsCount = 59;
 
-function countdown() {
-    seconds.textContent = secondsCount;
+    let interval = setInterval(countdown, 1000);
 
-    if (secondsCount == 0) {
-        secondsCount = 59;
-        minutesCount--;
-    } else {
-        secondsCount--;
+    function countdown() {
+        seconds.textContent = secondsCount;
+        setTimeout(() => {
+            console.log(secondsContainer);
+            secondsContainer.classList.add('flipped');
+        }, 100);
+
+        if (secondsCount === 0) {
+            secondsCount = 59;
+            minutesCount--;
+
+            if (minutesCount === 0) {
+                minutesCount = 59;
+                hoursCount--;
+
+                if (hoursCount === 0) {
+                    hoursCount = 23;
+                    daysCount--;
+
+                    if (daysCount === 0) {
+                        clearInterval(interval);
+                        alert("Lift off!!");
+                    }
+                }
+
+                flipCard('hoursContainer');
+            }
+
+            flipCard('minutesContainer');
+        } else {
+            secondsCount--;
+        }
+
+        days.textContent = daysCount;
+        hours.textContent = hoursCount;
+        minutes.textContent = minutesCount;
     }
-
-    minutes.textContent = minutesCount;
-
-    if (minutesCount == 0 && secondsCount == 59) {
-        minutesCount = 59;
-        hoursCount--;
-    }
-
-    hours.textContent = hoursCount;
-
-    if (hoursCount == 0 && minutesCount == 59 && secondsCount == 59) {
-        hoursCount = 23;
-        daysCount--;
-    }
-
-    days.textContent = daysCount;
-
-    if (daysCount == 0 && hoursCount == 0 && minutesCount == 59 && secondsCount == 59) {
-        clearInterval(interval);
-        alert("Lift off!!");
-    }
-}
+});
